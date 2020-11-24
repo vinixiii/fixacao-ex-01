163053dddd1bbd71070b9cf80fe6e20c8a4c8c3b
Vinícius <vinissfigueiroa@gmail.com>
2020-11-23 23:41:33 -0300

    Exercícío 01 - Feito

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace fixacao_ex_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoAtual = 2020;
+
+            Console.WriteLine("Digite a data de nascimento: ");
+            int nascimento = int.Parse(Console.ReadLine());
+            int validation = anoAtual - nascimento;
+
+            if(validation < 18){
+                Console.WriteLine("Você não pode votar esse ano.");
+            } else {
+                Console.WriteLine("Você pode votar esse ano.");
+            }
+        }
+    }
+}
